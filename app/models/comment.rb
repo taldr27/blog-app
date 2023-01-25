56d4e99130d7d320ba27797068639e1682c8e4fe
lana <diegogarciaq98@gmail.com>
2023-01-25 11:51:29 -0500

    Add method to latest posts in user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, foreign_key: 'author_id' class_name: 'User'
   belongs_to :post, foreign_key: 'posts_id', class_name: 'Post'
 end
